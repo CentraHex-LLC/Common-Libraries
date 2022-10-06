1c5a0b547cf9aca40007fb8bd9964fe318c8bcc8
Grey Johnson <contact@greyjohnson.dev>
2022-10-06 11:06:27 -0600

    Add Utility File

---
+++ Utility.ts
@@ -0,0 +1,38 @@
+export class Utility {
+  public static randomize(array: unknown[]) {
+    array.forEach(
+      (value, index) => {
+        const swapIndex = Math.floor(Math.random() * array.length);
+        const swapValue = array[swapIndex];
+        array[swapIndex] = value;
+        array[index] = swapValue; 
+      }
+    );
+  }
+  
+  public static range(count: number) {
+    return [...new Array(count).keys()];
+  }
+
+  public static resolvedPromise<T>(value: T): Promise<T> {
+    return new Promise(resolve => resolve(value));
+  }
+
+  public static rejectedPromise<T>(value: T): Promise<T> {
+    return new Promise((resolve, reject) => reject(value));
+  }
+
+  public static allTrue(values: boolean[]): boolean {
+    return values.reduce(
+      (previousValue, currentValue) => previousValue && currentValue
+    );
+  }
+
+  public static async sleep(duration: number) {
+    await new Promise(r => setTimeout(r, duration));
+  }
+
+  public static MinutesToMilliseconds(minutes: number) {
+    return 1000 * 60 * minutes;
+  }
+}
